ca6c353a0223ed9a80dfee5b5127c2eaa5378ca2
pemesie <gnchouwat@huskers.unl.edu>
2020-04-08 12:41:14 -0500

    Delete the previous work and start over

---
+++ main.js
@@ -0,0 +1,56 @@
+
+
+
+$(document).ready(function(){
+
+
+    let previousPointer = 1;
+    let nextPointer = 3;
+    const $element = $(".slideshowContainer");
+    // Find number of sliders in the div. Substract the 2 last children which aren't sliders.
+    const nberOfSliders =  $(".slideshowContainer").children().length - 2;
+
+    console.log( nberOfSliders);
+
+
+// ----------- Functions definition -------
+    function actionOnSliderPrevious(){
+        if(previousPointer >= 1 & previousPointer < nberOfSliders - 2){
+            //hide slider # previousPointer
+            $($element).find(`.slide${previousPointer}`).css("display", "none") ;
+            // display slider # previousPointer+2
+            $($element).find(`.slide${previousPointer + 3}`).css("display", "block") ;
+            previousPointer++;
+            nextPointer++;
+
+        }
+    }
+
+    function actionOnSliderNext(){
+        if(nextPointer > 3 & nextPointer < nberOfSliders + 1){
+            //display slider # nextPointer + 1
+            $($element).find(`.slide${nextPointer}`).css("display", "none");
+            //hide slide # nextPointer - 2
+            $($element).find(`.slide${nextPointer - 3}`).css("display", "block");
+            nextPointer--;
+            previousPointer--;
+        }
+    }
+    
+    init = function(){
+        let $previousElement = $("#prev");
+        let $nextElement = $("#next");
+    
+        $previousElement.on("click", actionOnSliderPrevious);
+        $nextElement.on("click", actionOnSliderNext);
+    }
+    
+    // window.addEventListener("load", init);
+
+
+
+    init();
+});
+
+
+
